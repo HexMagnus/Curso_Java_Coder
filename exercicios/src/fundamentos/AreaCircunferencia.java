53c942a6b1b29f3ef6d1330816177b8dcec167ac
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-04 20:49:34 -0300

    Update

---
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+	
+	public static void main(String[] args) {
+		double raio =  3.4;
+		final double PI = 3.14159;
+	
+		
+		double area = PI * raio * raio;
+		
+		
+		System.out.println(area);
+	}
+	
+}
+
+
+//Variavel é: Tipo, nome e valor. Linha 6.
+//Constante: Adicionando final antes da variavel ela se torna uma constante. Constantes sempre tem as letra MAISCULAS.
