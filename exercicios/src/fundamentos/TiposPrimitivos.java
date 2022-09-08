d0fcbf5a177811c5f97545f3bc2f3ae60e7b7bfa
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-08 20:47:45 -0300

    Update

---
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		//Informações do funcionario
+		
+		//Tipos númericos inteiros
+		byte anosDeEmpresa=23;
+		short numeroDeVoos=542;
+		int id = 56789;
+		
+	}
+}
