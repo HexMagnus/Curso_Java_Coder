c11077a425baa94066de466ae0084fd7cc5467ba
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-15 16:22:15 -0300

    Update TipoString.java

--- exercicios/src/fundamentos/TipoString.java
+++ exercicios/src/fundamentos/TipoString.java
@@ -3,6 +3,11 @@ package fundamentos;
 public class TipoString {
 
 	public static void main(String[] args) {
+		System.out.println("Olá pessoal".charAt(2));
 		
+		
+		String s = "Boa tarde";
+		System.out.println();
+		System.out.println();
 	}
 }
