f7d5ab493c35968a96a27aa676a9ff1948badf23
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-18 15:31:48 -0300

    Update

--- exercicios/src/fundamentos/Console.java
+++ exercicios/src/fundamentos/Console.java
@@ -1,5 +1,7 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class Console {
 
 	public static void main(String[] args) {
@@ -12,8 +14,11 @@ public class Console {
 		System.out.println("dia");
 
 		
-		System.out.printf("Megasena %d %d %d %d % d%d",1,2,3,4,5,6);
-		System.out.printf("Salário: %.1f" , 1234.5678);
+		System.out.printf("Megasena %d %d %d %d %d %d %n",1,2,3,4,5,6);
+		System.out.printf("Salário: %.1f%n" , 1234.5678);
+		System.out.printf("Nome: %s%n", "João");
+		
+		Scanner entrada = new Scanner(System.in);
 	}
 
 }
