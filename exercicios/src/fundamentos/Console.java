7074bd2ea92f4c7dbe7c69af5b839ab9219841ea
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-18 15:24:27 -0300

    Update Console.java

--- exercicios/src/fundamentos/Console.java
+++ exercicios/src/fundamentos/Console.java
@@ -5,12 +5,15 @@ public class Console {
 	public static void main(String[] args) {
 		
 		System.out.print("Bom");
-		System.out.print("dia");
+		System.out.print("dia\n\n");
 		
 		
-		System.out.println("Bom dia");
+		System.out.println("Bom");
 		System.out.println("dia");
 
+		
+		System.out.printf("Megasena", args);
+		
 	}
 
 }
