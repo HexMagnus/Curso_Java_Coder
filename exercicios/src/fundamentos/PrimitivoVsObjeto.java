1a07eca59c1386bf5406da804785df0817bcb982
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-21 16:46:25 -0300

    Update

---
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	
+	public static void main(String[] args) {
+		
+
+	String s = new String ("texto");
+	s.toUpperCase();
+	
+	int a = 123;
+	a.
+	
+	}
+
+}
