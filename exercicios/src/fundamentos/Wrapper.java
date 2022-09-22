15078969d3cbc6b54394a54e526a6bc7d0980b3d
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-22 00:03:34 -0300

    update

--- exercicios/src/fundamentos/Wrapper.java
+++ exercicios/src/fundamentos/Wrapper.java
@@ -17,11 +17,17 @@ public class Wrapper {
 		System.out.println(i * 3);
 		System.out.println(l /3);
 		
+		Float f = 123.0F;
+		System.out.println(f);
+		
+		Double d = 1234.5678;
+		System.out.println(d);
+		
 		Boolean bo = Boolean.parseBoolean("true");
 		System.out.println(bo);
 		System.out.println(bo.toString().toUpperCase());
 		
-		Character c = '#';
+		Character c = '#'; //char
 		System.out.println(c + "...");
 		
 
