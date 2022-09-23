e94cd00cd9095030d7539dbf934f74edd660aadb
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-23 19:42:19 -0300

    update

--- exercicios/src/fundamentos/ConversaoTipoPrimitivoNumerico.java
+++ exercicios/src/fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -5,7 +5,15 @@ public class ConversaoTipoPrimitivoNumerico {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double a = 1;
+		double a = 1; //implicita
+		System.out.println(a);
+		
+		float b = (float)1.12345; //explicita (CAST)
+		System.out.println(b);
+		
+		int c = 127;
+		byte d = (byte)c;
+		System.out.println(d);
 	}
 
 }
