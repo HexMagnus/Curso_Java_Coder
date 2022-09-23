847093cfc996dbd3625337e8bbbcb2d9e475364d
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-23 19:37:19 -0300

    Update

---
+++ exercicios/src/fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivoNumerico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 1;
+	}
+
+}
