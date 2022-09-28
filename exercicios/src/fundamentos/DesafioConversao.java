ae4cb4fe35cfda126679790d8528ebf261b384c2
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-28 18:25:24 -0300

    Update DesafioConversao.java

--- exercicios/src/fundamentos/DesafioConversao.java
+++ exercicios/src/fundamentos/DesafioConversao.java
@@ -8,13 +8,21 @@ public class DesafioConversao {
 		
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("Informe o primeiro salário");
-		String valor1 = entrada.next().replace(",", ",");
+		System.out.print("Informe o primeiro salário: R$ ");
+		String valor1 = entrada.next().replace(",", ".");
 		
+		System.out.print("\nInforme o segundo salário: R$ ");
+		String valor2= entrada.next().replace(",", ".");
 		
-		System.out.println();
+		System.out.print("\nInforme o terceiro salário: R$ ");
+		String valor3=entrada.next().replace(",", ".");
 				
-		System.out.println();
+		double salario1 = Double.parseDouble(valor1);
+		double salario2 = Double.parseDouble(valor2);
+		double salario3 = Double.parseDouble(valor3);
+		
+		double media = (salario1 + salario2 + salario3) / 3;
+		System.out.println("A média dos salários é R$ " + media);
 		
 		entrada.close();
 
