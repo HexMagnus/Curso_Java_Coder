cf2fbc3db37f3a73093c7aca162d90113b11019c
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-28 18:14:08 -0300

    Update

---
+++ exercicios/src/fundamentos/DesafioConversao.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro salário");
+		String valor1 = entrada.next().replace(",", ",");
+		
+		
+		System.out.println();
+				
+		System.out.println();
+		
+		entrada.close();
+
+	}
+
+}
