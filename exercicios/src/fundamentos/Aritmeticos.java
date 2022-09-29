9515bdf8a69eed2aa385c54830b7d1ea447dd49e
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-09-29 20:30:41 -0300

    Update Aritmeticos.java

--- exercicios/src/fundamentos/Aritmeticos.java
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -8,7 +8,20 @@ public class Aritmeticos {
 		
 		var x = 34.56;
 		double y = 2.2;
+		
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
 
+		int a = 8;
+		int b = 3;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		
 	}
 
 }
