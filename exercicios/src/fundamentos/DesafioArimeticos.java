91b1413528f3709018ec24639d91f0db640a41db
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-10-01 21:35:57 -0300

    update

--- exercicios/src/fundamentos/DesafioArimeticos.java
+++ exercicios/src/fundamentos/DesafioArimeticos.java
@@ -3,8 +3,22 @@ package fundamentos;
 public class DesafioArimeticos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double  numA =Math.pow(6 * (3 + 2), 2);
+		double  denA = 3 * 2;
+		
+		double numB = (1 -5) * (2 - 7);
+		double denB = 2;
+		
+		double superiorA = numA / numB;
+		double superiorB = Math.pow(numB / denB, 2);
+		
+		double superior = Math.pow(superiorA - superiorB, 3);
+		double inferior = Math.pow(10, 3);
+		
+		double resultado = superior / inferior;
+		
+		
+		
 	}
 
 }
