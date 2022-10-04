7b68efbc8e6f23f33362807a7e212643da50a352
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-10-04 19:41:26 -0300

    Update

--- exercicios/src/operadores/DesafiosLogicos.java
+++ exercicios/src/operadores/DesafiosLogicos.java
@@ -3,13 +3,25 @@ package operadores;
 public class DesafiosLogicos {
 
 	public static void main(String[] args) {
+		//Trabalho na terça(V ou F)
+		//Trabalho na quinta(V ou F)
 		
 		
-		boolean trabalho1 = false;
-		boolean trabalho2 = false;
+		boolean trabalho1 = true;
+		boolean trabalho2 = true;
 		
-		boolean comprouTv50 = trabalho1 && trabalho2;
-
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		boolean comprouTV32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		
+		
+		System.out.println("Comprou TV 50\"? " + comprouTV50); // barra na frente indica "ESCAPE"
+		System.out.println("Comprou TV 50\"? " + comprouTV32);
+		System.out.println("Comprou TV 32\" Sorvete? " + comprouSorvete);
+		
+		
+		//Operador Unário!
+		System.out.println("Mais saudávek" + !comprouSorvete);
 	}
 
 }
