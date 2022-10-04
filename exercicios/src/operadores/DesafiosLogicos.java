6ff823e44b93d6c16711d53d47b9340778e6f8bf
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-10-04 19:33:58 -0300

    Update

---
+++ exercicios/src/operadores/DesafiosLogicos.java
@@ -0,0 +1,15 @@
+package operadores;
+
+public class DesafiosLogicos {
+
+	public static void main(String[] args) {
+		
+		
+		boolean trabalho1 = false;
+		boolean trabalho2 = false;
+		
+		boolean comprouTv50 = trabalho1 && trabalho2;
+
+	}
+
+}
