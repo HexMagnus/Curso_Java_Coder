45f122ae8241692baeeb7d6e60ffb135aed85567
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-10-05 00:05:22 -0300

    Update

--- exercicios/src/operadores/DesafiosLogicos.java
+++ exercicios/src/operadores/DesafiosLogicos.java
@@ -8,22 +8,18 @@ public class DesafiosLogicos {
 		
 		
 		boolean trabalho1 = true;
-		boolean trabalho2 = true;
+		boolean trabalho2 = false;
 		
 		boolean comprouTV50 = trabalho1 && trabalho2;
 		boolean comprouTV32 = trabalho1 ^ trabalho2;
 		boolean comprouSorvete = trabalho1 || trabalho2;
-		
+		//Operador Unário!
 		boolean maisSaudavel = !comprouSorvete;
 		
-		
 		System.out.println("Comprou TV 50\"? " + comprouTV50); // barra na frente indica "ESCAPE"
-		System.out.println("Comprou TV 50\"? " + comprouTV32);
-		System.out.println("Comprou TV 32\" Sorvete? " + comprouSorvete);
-		
-		
-		//Operador Unário!
-		System.out.println("Mais saudávek" + !comprouSorvete);
+		System.out.println("Comprou TV 32\"? " + comprouTV32);
+		System.out.println("Comprou Sorvete? " + comprouSorvete);
+		System.out.println("Mais saudável? " + maisSaudavel);
 	}
 
 }
