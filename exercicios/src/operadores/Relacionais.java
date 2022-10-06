45f1bb1c9db7ee59acf93e3e66ccff82b3b6d068
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-10-06 18:03:04 -0300

    Update Relacionais.java

--- exercicios/src/operadores/Relacionais.java
+++ exercicios/src/operadores/Relacionais.java
@@ -15,6 +15,14 @@ public class Relacionais {
 		System.out.println(3 < 7);
 		System.out.println(30 <=7);
 		System.out.println(30 !=7);
+		
+		double nota = 9.9;
+		boolean bomComportamento = false;
+		boolean passouPorMedia = nota >=7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		
+		System.out.println("Tem desconto" + temDesconto);
+		
 	}
 
 }
